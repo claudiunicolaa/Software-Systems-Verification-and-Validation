93b8562e571a0112970bb0c60a903f1a963c006d
Claudiu Nicola <claudiunicola96@gmail.com>
2018-03-08 18:01:37 +0200

    I added the first name on employee class

--- src/main/java/ncir1923MV/model/Employee.java
+++ src/main/java/ncir1923MV/model/Employee.java
@@ -6,36 +6,65 @@ import ncir1923MV.validator.EmployeeValidator;
 
 public class Employee {
 
-    /** The last name of the employee */
+    /**
+     * The first name of the employee
+     */
+    private String firstName;
+
+    /**
+     * The last name of the employee
+     */
     private String lastName;
 
-    /** The unique id of the employee */
+    /**
+     * The unique id of the employee
+     */
     private String cnp;
 
-    /** The didactic function of the employee inside the university */
+    /**
+     * The didactic function of the employee inside the university
+     */
     private DidacticFunction function;
 
-    /** The salary of the employee */
+    /**
+     * The salary of the employee
+     */
     private String salary;
 
     /**
      * Default constructor for employee
      */
     public Employee() {
-        this.lastName  = "";
-        this.cnp       = "";
-        this.function  = DidacticFunction.ASISTENT;
-        this.salary    = "";
+        this.lastName = "";
+        this.cnp = "";
+        this.function = DidacticFunction.ASISTENT;
+        this.salary = "";
     }
 
     /**
      * Constructor with fields for employee
      */
     public Employee(String lastName, String cnp, DidacticFunction function, String salary) {
-        this.lastName  = lastName;
-        this.cnp       = cnp;
-        this.function  = function;
-        this.salary    = salary;
+        this.lastName = lastName;
+        this.cnp = cnp;
+        this.function = function;
+        this.salary = salary;
+    }
+
+    /**
+     * Getter for the employee first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Setter for the employee first name
+     *
+     * @param firstName the first name to be set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     /**
@@ -121,21 +150,18 @@ public class Employee {
      * equals function for employee
      */
     public boolean equals(Employee comparableEmployee) {
-        boolean hasSameLastName  = this.lastName.equals(comparableEmployee.getLastName()),
-                hasSameCNP       = this.cnp.equals(comparableEmployee.getCnp()),
-                hasSameFunction  = this.function.equals(comparableEmployee.getFunction()),
-                hasSameSalary    = this.salary.equals(comparableEmployee.getSalary());
+        boolean hasSameLastName = this.lastName.equals(comparableEmployee.getLastName()),
+                hasSameCNP = this.cnp.equals(comparableEmployee.getCnp()),
+                hasSameFunction = this.function.equals(comparableEmployee.getFunction()),
+                hasSameSalary = this.salary.equals(comparableEmployee.getSalary());
         return hasSameLastName && hasSameCNP && hasSameFunction && hasSameSalary;
     }
 
     /**
      * Returns the Employee after parsing the given line
      *
-     * @param _employee
-     *            the employee given as String from the input file
-     * @param line
-     *            the current line in the file
-     *
+     * @param _employee the employee given as String from the input file
+     * @param line      the current line in the file
      * @return if the given line is valid returns the corresponding Employee
      * @throws EmployeeException
      */
@@ -144,23 +170,23 @@ public class Employee {
 
         String[] attributes = _employee.split("[;]");
 
-        if( attributes.length != 5 ) {
+        if (attributes.length != 5) {
             throw new EmployeeException("Invalid line at: " + line);
         } else {
             EmployeeValidator validator = new EmployeeValidator();
             employee.setLastName(attributes[1]);
             employee.setCnp(attributes[2]);
 
-            if(attributes[3].equals("ASISTENT"))
+            if (attributes[3].equals("ASISTENT"))
                 employee.setFunction(DidacticFunction.ASISTENT);
-            if(attributes[3].equals("LECTURER"))
+            if (attributes[3].equals("LECTURER"))
                 employee.setFunction(DidacticFunction.LECTURER);
-            if(attributes[3].equals("TEACHER"))
+            if (attributes[3].equals("TEACHER"))
                 employee.setFunction(DidacticFunction.TEACHER);
 
             employee.setSalary(attributes[4]);
 
-            if( !validator.isValid(employee) ) {
+            if (!validator.isValid(employee)) {
                 throw new EmployeeException("Invalid line at: " + line);
             }
         }
